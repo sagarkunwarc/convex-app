3bc1f7a5aab5a50d6e71deb5299b7b4734768346
sagarkunwar <sagar.kunwar24@gmail.com>
2024-12-15 20:28:54 -0500

    project setup

---
+++ convex/queries.ts
@@ -0,0 +1,44 @@
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+
+export const createBooks = mutation({
+  args: { title: v.string(), author: v.string() },
+  handler: async (ctx, args) => {
+    const newBookId = await ctx.db.insert("books", {
+      title: args.title,
+      author: args.author,
+      isCompleted: false,
+    });
+    return newBookId;
+  },
+});
+
+
+//read
+export const getBooks = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("books").collect();
+  },
+});
+
+
+//update
+export const updateStatus = mutation({
+  args: { id: v.id("books"), isCompleted: v.boolean() },
+  handler: async (ctx, args) => {
+    const { id } = args;
+    await ctx.db.patch(id, { isCompleted: args.isCompleted });
+    return "updated";
+  },
+});
+
+//delete
+export const deleteBooks = mutation({
+  args: { id: v.id("books") },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+    return "deleted";
+  },
+});
---
+++ src/component/Books.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+import "../styles/book.css";
+import { book } from "../types/book.type";
+
+export const Books = ({ books }: { books: book[] }) => {
+  const [update, setUpdate] = useState(false);
+  const [id, setId] = useState("");
+
+  const deleteBooks = useMutation(api.queries.deleteBooks);
+  const updateStatus = useMutation(api.queries.updateStatus);
+
+  const handleClick = (id: string) => {
+    setId(id);
+    setUpdate(!update);
+  };
+
+  const handleDelete = (id: string) => {
+    deleteBooks({ id: id as Id<"books"> })
+      .then((mess) => console.log(mess))
+      .catch((err) => console.log(err));
+  };
+
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
+    e.preventDefault();
+    const formdata = new FormData(e.currentTarget);
+    const isCompleted: boolean =
+      (formdata.get("completed") as string) === "true";
+    updateStatus({ id: id as Id<"books">, isCompleted })
+      .then((mess) => console.log(mess))
+      .catch((err) => console.log(err));
+    setUpdate(false);
+  };
+
+  return (
+    <div>
+      {books.map((data: book, index: number) => {
+        return (
+          <div
+            key={data._id}
+            className={`book-container ${data.isCompleted ? "completed" : "not-completed"}`}
+          >
+            <h3>Book no: {index + 1}</h3>
+            <p>Book title: {data.title}</p>
+            <p>Book Author: {data.author}</p>
+            <p>
+              Completed Status:{" "}
+              {data.isCompleted ? "Completed" : "Not Completed"}
+            </p>
+            <button onClick={() => handleClick(data._id)}>Update</button>
+            {id === data._id && update && (
+              <>
+                <form onSubmit={(e) => handleUpdate(e, data._id)}>
+                  <select name="completed">
+                    <option value="true">Completed</option>
+                    <option value="false">Not Completed</option>
+                  </select>
+                  <input type="submit" />
+                </form>
+              </>
+            )}
+            <button onClick={() => handleDelete(data._id)}>delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
